1e12b71a7833d29d79528bdc32475960fdbd7d1c
mkaspriv <mkaspriv@deloitte.com>
2025-02-11 16:19:51 +0100

    chore: add boilerplate

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      'unused-imports': unusedImports,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      // removing unused imports
+      'unused-imports/no-unused-imports': 'warn',
+
+      // auto removing unused vars but keep function arguments
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { vars: 'all', args: 'none', ignoreRestSiblings: true },
+      ],
+
+      // imports sorting
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+
+      // Prettier
+      'prettier/prettier': 'warn',
+    },
+  },
+);
